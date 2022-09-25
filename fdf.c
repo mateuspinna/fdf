17f6a3f92caf0c01ea14880c644b43fc8c07eabe
Mateus Pinna Lousa <mateusteste@Diegos-MacBook-Pro.local>
2022-09-25 15:22:09 -0300

    nao sei o que ta rolando

--- fdf.c
+++ fdf.c
@@ -2,7 +2,6 @@
 
 typedef struct s_map
 {
-    int **map_array;
     int *lines;
     int *columns;
 } t_map;
@@ -19,7 +18,7 @@ int map_lines(char *str)
     return(line_counter);
 }
 
-void    fill_map_array(char *line, int *map_array, int *columns)
+void    fill_map_array(char *line, t_map map_info, int j, int **map_array)
 {
     char    **splitted_line;
     int     i;
@@ -28,51 +27,76 @@ void    fill_map_array(char *line, int *map_array, int *columns)
     splitted_line = ft_split(line, ' ');
     while (splitted_line[i])
         i++;
-    if (!(*columns))
-        *columns = i;
+    if (!(*map_info.columns))
+        *map_info.columns = i;
     free(splitted_line[i--]);
-    map_array = malloc(i * sizeof(int));
+    map_array[j] = malloc(i * sizeof(int));
     while (i)
     {
-        map_array[i] = ft_atoi(splitted_line[i]); 
+        map_array[j][i] = ft_atoi(splitted_line[i]); 
         free(splitted_line[i--]);
     }
     free(splitted_line);
     free(line);
 }
 
-void    map_read(char *str, t_map map_info)
+int **map_read(char *str, t_map map_info)
 {
     int     fd;
     char    *line;
     int     i;
+    int     **map_array;
 
     i = 0;
     *map_info.lines = map_lines(str);
     fd = open(str, O_RDONLY);
     if (fd == -1 || *map_info.lines == 0)
         exit(1);
-    map_info.map_array = malloc((*map_info.lines) * sizeof(int *));
-    while (i < (*map_info.lines))
+    map_array = malloc((*map_info.lines) * sizeof(int *));
+    if (!map_array)
+        exit(1);
+    while (i < *map_info.lines)
     {
         line = get_next_line(fd);
-        fill_map_array(line, map_info.map_array[i++], map_info.columns);
+        fill_map_array(line, map_info, i++, map_array);
     }
     close(fd);
+    return (map_array);
+}
+
+void    freeinfo(t_map map_info, int **map_array)
+{
+    int x;
+    int i;
+    int j;
+
+    i = 0;
+    x = *map_info.lines;   
+    while ( i < x )
+    {   
+        j = 0;
+        while (j < *map_info.columns)
+            ft_printf("%i ", (map_array[i][j++]));
+        free(map_array[i]);
+        ft_printf("\n");
+        i++;
+    }
+    free(map_array);
+    free (map_info.lines);
+    free (map_info.columns);
 }
 
 int main (int argc, char **argv)
 {
-    t_map map_info;
-
+    t_map   map_info;
+    int     **map_array;
     if (argc == 2)
     {
         map_info.lines = malloc(sizeof(int)); 
         map_info.columns = malloc(sizeof(int)); 
         ft_bzero(&(*map_info.lines), sizeof (int));
         ft_bzero(&(*map_info.columns), sizeof (int));
-        map_read(argv[1], map_info);
-        ft_printf("%i\n", *map_info.lines);
-        ft_printf("%i\n", *map_info.columns);
+        map_array = map_read(argv[1], map_info);
+        freeinfo(map_info, map_array);
     }
 }
